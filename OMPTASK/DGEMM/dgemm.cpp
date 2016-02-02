0cde03bd6cdb1bb7d9632850156fe624602b7a9f
Jeremy Kemp <jakemp@uh.edu>
2016-02-01 18:00:17 -0600

    reworked the task creation

--- OMPTASK/DGEMM/dgemm.cpp
+++ OMPTASK/DGEMM/dgemm.cpp
@@ -74,12 +74,10 @@ Stored blocks of B in transpose form, November 2007
 #ifndef BOFFSET
 #define BOFFSET 12
 #endif
-#define AA_arr(i,j) AA[(i)+(block+BOFFSET)*(j)]
-#define BB_arr(i,j) BB[(i)+(block+BOFFSET)*(j)]
-#define CC_arr(i,j) CC[(i)+(block+BOFFSET)*(j)]
 #define  A_arr(i,j)  A[(i)+(order)*(j)]
 #define  B_arr(i,j)  B[(i)+(order)*(j)]
 #define  C_arr(i,j)  C[(i)+(order)*(j)]
+#define  tempC_arr(i,j)  tempC[(i)+(order)*(j)]
 
 #define forder (1.0*order)
 
@@ -129,20 +127,15 @@ main(int argc, char **argv) {
         block = atoi(*++argv);
     } else block = DEFAULTBLOCK;
 
-    double *A1 = new double[order*order];
-    double *B1 = new double[order*order];
-    double *C1 = new double[order*order];
-
-    double *A2 = new double[order*order];
-    double *B2 = new double[order*order];
-    double *C2 = new double[order*order];
-    if (!A1 || !B1 || !C1 || !A2 || !B2 || !C2) {
+    double     *A = new double[order*order];
+    double     *B = new double[order*order];
+    double     *C = new double[order*order];
+    double *tempC = new double[order*order];
+    if (!A || !B || !C || !tempC) {
         printf("ERROR: Could not allocate space for global matrices\n");
         exit(EXIT_FAILURE);
     }
 
-    double *A = A1, *B = B1, *C = C1;
-    double *A_next = A2, *B_next = B2, *C_next = C2;
 
     ref_checksum = (0.25*forder*forder*forder*(forder-1.0)*(forder-1.0));
 
@@ -154,6 +147,7 @@ main(int argc, char **argv) {
                 A_arr(i,j) = (double) j;
                 B_arr(i,j) = (double) j; 
                 C_arr(i,j) = 0.0;
+                tempC_arr(i,j) = 0.0;
             }
         }
 #pragma omp master 
@@ -172,54 +166,33 @@ main(int argc, char **argv) {
                 }
                 for(int jj = 0; jj < order; jj+=block) {
                     for(int kk = 0; kk < order; kk+=block) {
-#pragma omp task depend(out: BB_arr(jj,kk))
-                        for (int jg=jj,j=0; jg<MIN(jj+block,order); j++,jg++) {
-                            for (int kg=kk,k=0; kg<MIN(kk+block,order); k++,kg++) {
-                                BB_arr(j,k) =  B_arr(kg,jg);
-                            }
-                        }
                         for(int ii = 0; ii < order; ii+=block) {
-#pragma omp task depend(out: AA_arr(ii,kk))
-                            for (int kg=kk,k=0; kg<MIN(kk+block,order); k++,kg++) {
-                                for (int ig=ii,i=0; ig<MIN(ii+block,order); i++,ig++) {
-                                    AA_arr(i,k) = A_arr(ig,kg);
-                                }
-                            }
-#pragma omp task depend(out: CC_arr(0,0))
-                            for (int jg=jj,j=0; jg<MIN(jj+block,order); j++,jg++) {
-                                for (int ig=ii,i=0; ig<MIN(ii+block,order); i++,ig++) {
-                                    CC_arr(i,j) = 0.0;
-                                }
-                            }
-#pragma omp task depend(out: CC_arr(0,0)) depend(in: AA_arr(0,0), BB_arr(0,0), CC_arr(0,0)) 
-                            for (int kg=kk,k=0; kg<MIN(kk+block,order); k++,kg++) {
-                                for (int jg=jj,j=0; jg<MIN(jj+block,order); j++,jg++)  {
-                                    for (int ig=ii,i=0; ig<MIN(ii+block,order); i++,ig++) {
-                                        CC_arr(i,j) += AA_arr(i,k)*BB_arr(j,k);
+#pragma omp task depend(inout: C_arr(ii, jj))
+                            {
+                                for (int j=jj; j<MIN(jj+block,order); j++) {
+                                    for (int i=ii; i<MIN(ii+block,order); i++) {
+                                        tempC_arr(i,j) = 0.0;
                                     }
                                 }
-                            }
-#pragma omp task depend(out: C_arr(ii,jj)) depend(in: CC_arr(0,0)) 
-                            for (int jg=jj,j=0; jg<MIN(jj+block,order); j++,jg++) {
-                                for (int ig=ii,i=0; ig<MIN(ii+block,order); i++,ig++) {
-                                    C_arr(ig,jg) += CC_arr(i,j);
+                                for (int kg=kk; kg<MIN(kk+block,order); kg++) {
+                                    for (int jg=jj; jg<MIN(jj+block,order); jg++)  {
+                                        for (int ig=ii; ig<MIN(ii+block,order); ig++) {
+                                            tempC_arr(ig,jg) += A_arr(ig,kg)*B_arr(jg,kg);
+                                        }
+                                    }
+                                }
+//#pragma omp task depend(out: C_arr(ii,jj)) depend(in: CC_arr(0,0)) 
+                                for (int jg=jj,j=0; jg<MIN(jj+block,order); j++,jg++) {
+                                    for (int ig=ii,i=0; ig<MIN(ii+block,order); i++,ig++) {
+                                        C_arr(ig,jg) += tempC_arr(i,j);
+                                    }
                                 }
                             }
                         }
                     }  
                 }
-#pragma omp taskwait
-/*
-#pragma omp for 
-                for (int jg=0; jg<order; jg++) {
-                    for (int kg=0; kg<order; kg++) {
-                        for (int ig=0; ig<order; ig++) {
-                            C_arr(ig,jg) += A_arr(ig,kg)*B_arr(kg,jg);
-                        }
-                    }
-                }
-*/
             } // end of iterations
+#pragma omp taskwait
             dgemm_time = wtime() - dgemm_time;
         }//end of Master
     } // end of parallel region
